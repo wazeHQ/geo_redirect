7e4e8c39ed91796fdfd7e7756b0171ae04ca95d3
Sagie Maoz <sagie@maoz.info>
2013-04-12 11:11:04 +0300

    Code cleanup

--- spec/geo_redirect_spec.rb
+++ spec/geo_redirect_spec.rb
@@ -8,18 +8,18 @@ describe GeoRedirect do
       mock_app
 
       @app.config.should_not be_nil
-      @app.config.should eq YAML.load_file("spec/fixtures/config.yml")
+      @app.config.should eq YAML.load_file(fixture_path("config.yml"))
     end
 
     it "raises on not-found config file" do
       expect {
-        mock_app :config => '/no_such_file'
+        mock_app :config => "/no_such_file"
       }.to raise_error
     end
 
     it "raises on a mal-formatted config file" do
       expect {
-        mock_app :config => 'spec/fixtures/config.bad.yml'
+        mock_app :config => fixture_path("config.bad.yml")
       }.to raise_error
     end
   end
@@ -34,14 +34,14 @@ describe GeoRedirect do
 
     it "raises on not-found db file" do
       expect {
-        mock_app :db => '/no_such_file'
+        mock_app :db => "/no_such_file"
       }.to raise_error
     end
 
     it "raises on mal-formatted db file" do
       pending "GeoIP does not raise on bad files"
       expect {
-        mock_app :db => 'spec/fixtures/config.yml'
+        mock_app :db => fixture_path("config.yml")
       }.to raise_error
     end
   end
