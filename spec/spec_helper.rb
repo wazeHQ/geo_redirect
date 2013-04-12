336122f2804b83aec0c01cdbd25f28bc1c6621c0
Sagie Maoz <sagie@maoz.info>
2013-04-12 12:44:34 +0300

    Move support module to support.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
+require "rspec"
 require "rack"
+
 require_relative "../lib/geo_redirect.rb"
+require "support"
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
@@ -8,36 +11,4 @@ RSpec.configure do |config|
   config.filter_run :focus
 
   config.order = "random"
-end
-
-module GeoRedirect
-  module Support
-    def fixture_path(file)
-      "spec/fixtures/#{file}"
-    end
-
-    def nonexisting_file_path
-      "/no_such_file"
-    end
-
-    def app; Rack::Lint.new(@app); end
-
-    def mock_app(options = {})
-      options = { :config => fixture_path("config.yml"),
-                  :db => fixture_path("GeoIP.dat")
-                }.merge(options)
-
-      # Simple HTTP server that always returns 'Hello world!'
-      main_app = lambda { |env|
-        Rack::Request.new(env)
-        headers = {"Content-Type" => "text/html"}
-        [200, headers, ["Hello world!"]]
-      }
-
-      builder = Rack::Builder.new
-      builder.use GeoRedirect::Middleware, options
-      builder.run main_app
-      @app = builder.to_app
-    end
-  end
 end
