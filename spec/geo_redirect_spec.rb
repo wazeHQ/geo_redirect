ec8eeaa7d52891ccfa68bff3e1f95026f20d85a6
Sagie Maoz <sagie@maoz.info>
2013-04-12 10:47:14 +0300

    examples for load_db

--- spec/geo_redirect_spec.rb
+++ spec/geo_redirect_spec.rb
@@ -4,7 +4,6 @@ describe GeoRedirect do
   include GeoRedirect::Support
 
   describe "#load_config" do
-
     it "reads a config file successfully" do
       mock_app
 
@@ -16,16 +15,36 @@ describe GeoRedirect do
         :default => { :host => 'biz-world.waze.com' },
       })
     end
-
     it "raises on not-found config file" do
       expect {
         mock_app :config => '/no_such_file'
       }.to raise_error
     end
-
     it "raises on a mal-formatted config file" do
       expect {
         mock_app :config => 'spec/fixtures/config.bad.yml'
+      }.to raise_error
+    end
+  end
+
+  describe "#load_db" do
+    it "reads a db file successfully" do
+      mock_app
+
+      @app.db.should_not be_nil
+      @app.db.should be_a_kind_of GeoIP
+    end
+
+    it "raises on not-found db file" do
+      expect {
+        mock_app :db => '/no_such_file'
+      }.to raise_error
+    end
+
+    it "raises on mal-formatted db file" do
+      pending "GeoIP does not raise on bad files"
+      expect {
+        mock_app :db => 'spec/fixtures/config.yml'
       }.to raise_error
     end
   end
