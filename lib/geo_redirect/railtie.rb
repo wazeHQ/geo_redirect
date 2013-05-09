2f8701008237dc9f5ab7a145d7601eeaee65e2c6
Sagie Maoz <sagie@maoz.info>
2013-05-09 13:26:39 +0300

    Rename tasks file to .rake

--- lib/geo_redirect/railtie.rb
+++ lib/geo_redirect/railtie.rb
@@ -4,7 +4,7 @@ require 'rails'
 module GeoRedirect
   class Railtie < Rails::Railtie
     rake_tasks do
-      require 'tasks/geo_redirect.rb'
+      require 'tasks/geo_redirect.rake'
     end
   end
 end
